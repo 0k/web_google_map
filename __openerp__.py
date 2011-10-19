f1be8a211012b655d16f3e6759f8fdd53879323c
Peter A. Kurishev <peter@infosreda.com>
2011-10-19 18:49:05 +0400

    Decription of the module changed

--- __openerp__.py
+++ __openerp__.py
@@ -26,29 +26,38 @@
     'description': """
 InfoSreda OpenERP Web Map
 =========================
-Web Map module brings Google Map support right into OpenERP web-client.
+Web Map module brings Google Map support right for OpenERP web-client.
 
 Usage
 =====
-Module usage is not a big talk. After installation, all "res.partner.address" and "res.partner" views will be extended with neatly map.
 
-Just type in an address and press "Get coordinates" button. Google Geocoder will populate latitude and longitude fields. Map marker will be set accordingly to them.
+After installation, all "res.partner.address" and "res.partner" views
+will be extended with neatly map.
 
-Not happy with geocoding result? Update lat and lng by dragging marker.
+Just type an address and press "Get coordinates" button. Google
+Geocoder will populate latitude and longitude fields. Map marker will
+be set accordingly to them.
 
-Save your model and enjoy.
+Not happy with geocoding result? Update lat and lng as simply as
+dragging marker over Google Map widget.
 
-So, you are module developer?
-=============================
-Good news! If you want to add Google Map support for your own module, all you need to do is just put the next string to the view.
+Save your data and enjoy.
+
+So, you are a module developer
+==============================
+
+Good news! In case you want to add Google Map support for your own
+module, all you need to do is just put the next string to the view.
+
 ::
 
     <field name="map" widget="gmap" />
 
-If you want to use custom model address instead of "res.partner.address" model, please modify gmap.js source file selectors.
-""",
+If you want to use custom model address instead of
+"res.partner.address" model, please modify gmap.js source file
+selectors.""",
     'author': 'Infosreda LLC',
-    'website': 'http://infosreda.com/',
+    'website': 'http://www.infosreda.com/',
     'depends': ['base'],
     'init_xml': ['gmap_view.xml'],
     'update_xml': [],
